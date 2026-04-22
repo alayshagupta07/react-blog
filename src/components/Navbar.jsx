27699e595ccbb8f96bca4e17eb515782277bed15
alaysha gupta <alayshagupta123@gmail.com>
2026-04-22 18:44:06 +0530

    correct upload

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import '../App.css'
+
+const Navbar = () => {
+  return (
+    <div>
+        
+            <Link to="/">Home</Link>
+            <Link to="/bollywood">Bollywood</Link>
+            <Link to="/hollywood">Hollywood</Link>
+            <Link to="/technology">Technology</Link>
+            <Link to="/fitness">Fitness</Link>
+            <Link to="/food">Food</Link>
+
+
+    </div>
+  )
+}
+
+export default Navbar
